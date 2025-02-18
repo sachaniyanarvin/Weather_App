9c6cc8705801e0f10e77ec326e3ac8e6be32ce91
sachaniyanarvin <sachaniyanarvin21@gmail.com>
2025-02-19 00:08:57 +0530

    fix error in main.jsx file

--- Weather/src/main.jsx
+++ Weather/src/main.jsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import Temp from './temp.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Temp />
   </StrictMode>,
 )
